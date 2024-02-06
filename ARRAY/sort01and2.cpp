342f03983e0fdea4c04cfd8a9e8dd91df3025fd9
PratikRaut5530 <pratikraut5630@gmail.com>
2024-02-07 00:05:01 +0530

    feb7

--- ARRAY/sort01and2.cpp
+++ ARRAY/sort01and2.cpp
@@ -39,6 +39,25 @@ void sortArray(vector<int>& arr, int n)
     }
 }
 
+//dutch national flag algo
+void sortduntfl(vector<int> &arr,int n){
+    int low =0,mid=0,high=n-1;
+    while(mid<=high){
+        if(arr[mid]==0){
+            swap(arr[low],arr[mid]);
+            low++;
+            mid++;
+        }
+        else if(arr[mid]==1){
+            mid++;
+        }
+        else{
+            swap(arr[mid],arr[high]);
+            high--;
+        }
+    }
+}
+
 int main()
 {
     vector<int> arr{2,2,2,2,0,0,1,0};
